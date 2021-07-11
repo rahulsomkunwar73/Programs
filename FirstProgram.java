fbfcc2e511bcdc128dc9c99ddf4b551304541215
rahuls <rahulsomkunwar@gmail.com>
2021-07-11 18:26:59 +0530

    latest push

--- FirstProgram.java
+++ FirstProgram.java
@@ -1,7 +1,21 @@
+import java.util.*;
+
+
 class FirstProgram{
 
 public static void main(String[] args) {
-System.out.println("here in main");
+Map mp = new HashMap<String,Integer>();
+    mp.put("Aahul",new Integer(1));
+        mp.put("Sam",new Integer(2));
+    mp.put("Rahuls",new Integer(3));
+    mp.put("zako",new Integer(4));
+
+
+  List ls =   mp.keySet().stream().map(e -> mp.get(e)).forEach(e -> System.out.println(e+" 0----> "+mp.get(e)));
+
+System.out.println("here in main --->  --"+ls.get(0));
+
+
 }
 
 // find square of number
